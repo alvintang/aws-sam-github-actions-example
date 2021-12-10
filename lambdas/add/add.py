56b0d4ff2e772ab696ccd329a0a48cca4e7aa9f8
Alvin Tang <alvin.tang@bellroy.com>
2021-12-10 11:16:12 +0800

    change add

--- lambdas/add/add.py
+++ lambdas/add/add.py
@@ -1,28 +1,15 @@
-# Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: MIT-0
-
 import json
 
 def lambda_handler(event, context):
    print(event)
-   first = event['input']['first']
-   second = event['input']['second']
-   third = event['input']['third']
-   result = event['input']['result']
+   first = event['first']
+   second = event['second']
    
-   print(f"FIRST: {first}, SECOND: {second}, THIRD: {third}")
-   
-   result = int(result) + int(second)
-   
+   result = int(first) + int(second)
    print(f"RESULT: {result}")
    
    response = {
-       "first": first,
-       "second": second,
-       "third": third,
        "result": int(result)
    }
    
-   event['input'] = response
-   
-   return event['input']
+   return response
